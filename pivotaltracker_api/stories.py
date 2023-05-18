076c3e70f3b44d05d2e88b3d2ebe727a1dd8db97
Diego Cuevas <diegocuevas579@gmail.com>
2023-05-18 16:57:20 -0500

    Add: endpoint to get all stories

--- pivotaltracker_api/stories.py
+++ pivotaltracker_api/stories.py
@@ -1,12 +1,19 @@
+from dotenv import load_dotenv
 import requests
+import json
+import os
 
-url = "https://www.pivotaltracker.com/services/v5/projects/1416286/stories?with_state=unstarted"
+class StoryAPI:
+    def __init__(self, base_url='https://www.pivotaltracker.com/services/v5/projects/1416286/'):
+        load_dotenv()
+        self.base_url = base_url
+        self.headers = {'X-TrackerToken': os.environ['TRACKER_TOKEN']}
 
-payload = {}
-headers = {
-  'X-TrackerToken': ''
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+    def get_all_stories(self, state=''):
+        try:
+            response = requests.get('{}stories?{}'.format(self.base_url, state), headers=self.headers)
+            response.raise_for_status() # raise exception if invalid response
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print("Request error:{}".format(e))
+            return None
