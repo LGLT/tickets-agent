bffe0ace7235c6753ae4b89800132da3fae8859a
LGLT <leonardo.lopez.jobs@gmail.com>
2023-05-18 19:50:16 -0600

    Added: find tickets by state, and update tickets state

--- pivotaltracker_api/stories.py
+++ pivotaltracker_api/stories.py
@@ -11,9 +11,25 @@ class StoryAPI:
 
     def get_all_stories(self, state=''):
         try:
-            response = requests.get('{}stories?{}'.format(self.base_url, state), headers=self.headers)
+            params = { 'with_state': state }
+            url = '{}stories?'.format(self.base_url)
+            response = requests.get(url, params=params, headers=self.headers)
             response.raise_for_status() # raise exception if invalid response
-            return response.json()
+            parsed_response = self.parse_response(response.json())
+            return parsed_response
         except requests.exceptions.RequestException as e:
             print("Request error:{}".format(e))
             return None
+    
+    def update_story(self, story_id, story_state):
+        try:
+            params = { 'current_state': story_state }
+            url = f'{self.base_url}stories/{story_id}'
+            requests.put(url, params=params, headers=self.headers)
+            return 
+        except requests.exceptions.RequestException as e:
+            print("Request error:{}".format(e))
+            return e
+    
+    def parse_response(self, tickets):
+        return [{'id':ticket['id'], 'name': ticket['name'], 'url': ticket['url'], 'labels': {label['name'] for label in ticket['labels']}} for ticket in tickets]
